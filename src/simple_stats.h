73c14078acc14faa05fd3e216b3c73e943a34134
Eric Herman <eric@freesa.org>
2016-10-09 14:46:12 +0200

    LGPLv3+
    copyright and boilerplate

--- src/simple_stats.h
+++ src/simple_stats.h
@@ -1,4 +1,21 @@
-/* simple_stats.h - Eric Herman 2014 */
+/* simple_stats.h stats library API
+   Copyright (C) 2014, 2016 Eric Herman <eric@freesa.org>
+
+   This work is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This work is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License and the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License (COPYING) and the GNU General Public License (COPYING.GPL3).
+   If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef SIMPLE_STATS_H
 #define SIMPLE_STATS_H
 
--- src/ss_options.h
+++ src/ss_options.h
@@ -1,5 +1,21 @@
+/* ss_options.h stats library options structure
+   Copyright (C) 2014, 2016 Eric Herman <eric@freesa.org>
 
-/* ss_options.h - Eric Herman 2014 */
+   This work is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This work is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License and the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License (COPYING) and the GNU General Public License (COPYING.GPL3).
+   If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef SS_OPTIONS_H
 #define SS_OPTIONS_H
 
